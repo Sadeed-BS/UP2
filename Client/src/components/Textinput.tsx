d5481a3dba651d5b5df61bdccf0b339b0e863895
Sadeed Bin Sadik <70870990+Sadeed-BS@users.noreply.github.com>
2025-08-09 15:56:05 +0530

    Last Frontend

--- Client/src/components/Textinput.tsx
+++ Client/src/components/Textinput.tsx
@@ -40,7 +40,7 @@ export const Demo = () => {
     setMessage("");
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/chat", {
+      const response = await fetch("https://up-fmhw.onrender.com/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
